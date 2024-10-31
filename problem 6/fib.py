698ed86e8f4d95d35ec8e28d9d2baf018ddeb148
cts9883 <cts9883@utulsa.edu>
2024-10-31 09:10:14 -0500

    Problem 6

--- problem 6/fib.py
+++ problem 6/fib.py
@@ -1,4 +1,5 @@
 def fibonacci(n):
+
     if n < 0:
         fib = -1
     elif n == 0:
@@ -6,12 +7,17 @@ def fibonacci(n):
     elif n == 1:
         fib = 1
     else:
-        while n>0:
-            num_1 = 0
-            num_2 = 1
+        num_1 = 0
+        num_2 = 1
+        while n > 1:
+            numsum = num_1 + num_2
+            num_1 = num_2
+            num_2 = numsum
+            n = n - 1
+            fib = num_2
+    return fib 
 
-
-    return fib
+  
 
 if __name__ == '__main__':
     start_num = int(input())
